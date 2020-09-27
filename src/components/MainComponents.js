4ff36b4ed2abe8428dc6f530594d06279809392f
Prateek Varshney <varshneyprateek20@gmail.com>
2020-09-27 11:09:20 +0530

    Single-Page-Application Part 1

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import {Navbar,NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponents';
 import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
 import Dishdetail from './dishdetailcomponent';
+import Contact from './ContactComponent';
 import Home from './HomeComponents';
 import {Switch,Route,Redirect} from 'react-router-dom';
 import Header from './HeaderComponents';
 import Footer from './FooterComponents';
+
 
 class Main extends Component {
 
@@ -14,7 +19,10 @@ class Main extends Component {
      super(props);
 
      this.state = {
-      dishes:DISHES
+      dishes:DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
      };
   }
 
@@ -22,7 +30,11 @@ class Main extends Component {
 
      const HomePage = () => {
        return (
-         <Home/>
+         <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
        );
       }
       
@@ -32,6 +44,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage}/>
           <Route path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Route path="/contactus" component={Contact} />
           <Redirect to="/home"/>
         </Switch>
         <Footer/>
