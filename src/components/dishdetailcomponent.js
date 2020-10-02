2e65b71a9efdefd41aa1fb84cc4321eb10285950
Prateek Varshney <varshneyprateek20@gmail.com>
2020-10-02 12:46:00 +0530

    Assignment 3

--- src/components/dishdetailcomponent.js
+++ src/components/dishdetailcomponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{Component} from "react";
 import {
   Card,
   CardImg,
@@ -6,10 +6,15 @@ import {
   CardBody,
   CardTitle,
   Breadcrumb,
-  BreadcrumbItem
+  BreadcrumbItem,
+  Button
 } from "reactstrap";
+import {Form,FormGroup,Label,Input,Modal,ModalHeader,ModalBody,Row,Col} from 'reactstrap';
 import { Link } from "react-router-dom";
+import {Control,LocalForm,Errors} from 'react-redux-form';
 
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
 
 function RenderDish({ dish }) {
   return (
@@ -43,6 +48,7 @@ function RenderComments({ comments }) {
             </li>
           </ul>
         ))}
+        <CommentForm/>
       </div>
     );
   }
@@ -67,7 +73,82 @@ const DishDetailComponent = props => (
       <RenderDish dish={props.dish} />
       <RenderComments comments={props.comments} />
     </div>
-  </div>
+  </div> );
 
-);
+class CommentForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen:false
+    }
+    this.toggleModal = this.toggleModal.bind(this);    
+    this.handleLogin = this.handleLogin.bind(this); 
+  }
+
+   handleLogin(values){
+     this.toggleModal();
+     alert("Rating: " + values.rating + " Name: " + values.name + " Comment : " + values.comment );
+  }
+
+  toggleModal() {
+     this.setState({isModalOpen:!this.state.isModalOpen}); 
+  }
+
+  render() {
+    return(
+       <div>
+       <Button outline onClick={this.toggleModal}>
+           <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+           <ModalHeader toggle={this.toggleModal} > Submit Comment</ModalHeader>
+           <ModalBody>
+                 <LocalForm onSubmit={(values) => this.handleLogin(values)}>
+                      <Row className="form-group">
+                         <Col md={10}>
+                          <p>Rating</p>
+                          <Control.select model=".rating" name="rating" 
+                            className="form-control" >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                          </Control.select>
+                         </Col>
+                      </Row>
+                      <Row className="form-group">
+                         <Col md={10}>
+                           <Label htmlFor="name" >Your Name</Label>
+                           <Control.text model=".name" id="name" name="name" 
+                              placeholder="Your Name"
+                              className="form-control" 
+                              validators={{
+                                minLength: minLength(3), maxLength: maxLength(15)
+                              }} />
+                              <Errors className="text-danger" model=".name" show="touched"
+                               messages={{
+                                 maxLength: ' Must be 15 Characters or less ',
+                                 minLength: ' Must be greater than 2 Characters '
+                               }} />
+                         </Col>
+                      </Row>
+                      <Row className="form-group">
+                          <Col md={10}>
+                           <Label htmlFor="comment" md={2}>Comment</Label>
+                             <Control.textarea model=".comment" id="comment" name="comment" 
+                              rows="6"
+                              className="form-control" />
+                          </Col>
+                      </Row>
+                      <Button type="submit" value="submit" color="primary">Login</Button>
+                 </LocalForm>
+           </ModalBody>
+       </Modal>
+       </div>  
+    );
+  }
+}
+
 export default DishDetailComponent;
